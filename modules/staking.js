bf2005f8373111c74ba8f30d0912ed3aa6c88bd3
apjproduction <apjproduction@gmail.com>
2024-02-25 07:39:38 -0800

    Add staking module

---
+++ modules/staking.js
@@ -0,0 +1,14 @@
+// staking.js
+function stakeTokens(userAddress, amount) {
+    // Logic for staking tokens
+}
+
+function unstakeTokens(userAddress, amount) {
+    // Logic for unstaking tokens
+}
+
+module.exports = {
+    stakeTokens,
+    unstakeTokens
+};
+
